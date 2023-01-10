5b41625a5ea30ff93ea4386ad791052144c43308
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2023-01-10 11:15:44 -0600

    props

--- People/person.py
+++ People/person.py
@@ -6,15 +6,48 @@ from traits import Sex
 class Person:
     def __init__(self):
         self.name = ""
-        self.dob:datetime.date = None
+        self.dob:datetime.date = None       #Date of Birth
+        self.dod:datetime.date = None       #Date of Death
         self.sex = Sex()
         # human traits
 
 
 
+
+
+
+    ''' Properties '''
     @property
     def name(self):
         return self._name
     @name.setter
     def name(self, newname):
         self._name = newname
+        
+    @property
+    def age(self):
+        return self._age
+    @age.setter
+    def age(self, newage):
+        self._age = newage
+
+    @property
+    def sex(self):
+        return self._sex
+    @sex.setter
+    def sex(self, newsex):
+        self._sex = newsex
+
+    @property
+    def dob(self):
+        return self._dob
+    @dob.setter
+    def dob(self, newdob):
+        self._dob = newdob
+
+    @property
+    def dod(self):
+        return self._dod
+    @dod.setter
+    def dod(self, newdod):
+        self._dod = newdod
