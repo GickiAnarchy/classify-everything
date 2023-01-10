e6e230ac21ae292d3e2f1a1150323c3855a6b890
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2023-01-10 15:20:45 +0000

    Idk anymore

--- People/person.py
+++ People/person.py
@@ -4,9 +4,17 @@ import datetime
 from traits import Sex
 
 class Person:
-  def __init__(self):
-    self.name = ""
-    self.dob:datetime.date = None
-    self.sex = Sex()
-    # human traita
-    
+    def __init__(self):
+        self.name = ""
+        self.dob:datetime.date = None
+        self.sex = Sex()
+        # human traits
+
+
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, newname):
+        self._name = newname
