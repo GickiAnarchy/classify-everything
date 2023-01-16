3111e021f2c3982c0da93fbf1d7d3dd879c6d581
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2023-01-16 17:03:57 +0000

    It's a changin

---
+++ People/actions.py
@@ -0,0 +1,8 @@
+
+from person import Person
+
+import random
+
+class Action:
+    pass
+
--- People/traits.py
+++ People/traits.py
@@ -1,7 +1,24 @@
 '''person traits'''
 
+import os
 
 class Sex:
-    def __init__(self, sex = "Unknown"):
+    def __init__(self, sex):
         self.sex = sex
+        if str.lower(sex) == "male":
+            self.sex = "Male"
+        if str.lower(sex) == "female":
+            self.sex = "Female"
+        else:
+            self.sex = "Unknown"
 
+    
+    @property
+    def can_have_children(self):
+        if self.sex == "Female":
+            return True
+        else:
+            return False
+
+
+    
